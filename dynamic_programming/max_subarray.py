ed8f1aa6ed615b3e95d07e3aeb98181ac9bce25c
Marcelle Bonterre <laanak@gmail.com>
2016-04-23 16:59:17 -0400

    skeleton for max subarray problem

---
+++ dynamic_programming/max_subarray.py
@@ -0,0 +1,12 @@
+def main():
+    for caseNum in range(0,int(raw_input())):
+        case_len = int(raw_input())
+        case = raw_input().split()
+        print max_subarray(case)
+
+def max_subarray(arr):
+    max_arr = []
+    
+    return max_arr
+
+main()
