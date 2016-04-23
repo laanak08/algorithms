b656891b8a8cea8919946c5af8ec37de768e6652
Marcelle Bonterre <laanak@gmail.com>
2016-04-23 11:31:09 -0400

    basic recursive fibonacci approach

---
+++ dynamic_programming/fibonacci_modified.py
@@ -0,0 +1,12 @@
+def main(work_func):
+    A,B,N = raw_input().split(" ")
+    print T(A,B,N)
+    
+def T(A,B,N):
+    if N == A:
+        return A
+    elif N == B:
+        return B
+    else:
+        return T(A,B,(N-1)) + T(A,B,(N-2))
+main()
